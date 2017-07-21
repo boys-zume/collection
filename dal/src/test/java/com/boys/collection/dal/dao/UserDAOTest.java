335c90983a4eaac2df78269f07f093d98cefcc4b
yangweijie <153503767@qq.com>
2017-07-22 01:11:04 +0800

    init

---
+++ dal/src/test/java/com/boys/collection/dal/dao/UserDAOTest.java
@@ -0,0 +1,13 @@
+package com.boys.collection.dal.dao;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by yangweijie on 2017/7/22.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserDAOTest {
+}
